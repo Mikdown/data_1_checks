60bccd1d5693b744b25be5ad336318ab5f5653cf
MikeDown <mikdowns@outlook.com>
2022-09-27 09:54:13 -0400

    Added additional code to complete the requirement.

--- data_1_check1.py
+++ data_1_check1.py
@@ -1,42 +1,23 @@
-"""1. Print "hello world" with single line of code.
-2. Print "hello world" with variable."""
-
-print('hello world')
+print('hello world')  # Print "hello world" with single line of code.
 # or like this:
 statement = 'hello world'
-print(statement)
+print(statement)  # Print "hello world" with variable.
 
-"""1. Create list with strings, floats, and integers.
-2. Confirm the list object type.
-3. Print the value of the 4th position in the list. """
+list_1 = ['list1', 'list2', 'list3', 1.10, 2.20, 3.30, 1, 2, 3]  # Create list with strings, floats, and integers.
+print(type(list_1))  # Confirm the list object type.
+print(list_1[3])  # Print the value of the 4th position in the list.
 
-list_1 = ['list1', 'list2', 'list3', 1.10, 2.20, 3.30, 1, 2, 3]
-print(type(list_1))
-print(list_1[3])
+dict_1 = {'first_key': '1st_val', 'second_key': 2}  # Create dictionary with strings and integers.
+print(type(dict_1))  # Confirm the dictionary object type.
+print(dict_1['first_key'])  # Print the value of the first_key in the dictionary.
 
-"""1. Create dictionary with strings and integers.
-2. Confirm the dictionary object type.
-3. Print the value of the first_key in the dictionary. """
+tuple_1 = ('1:00PM', 2.00, '3:PM', 4)  # Create a tuple with strings, floats, and integers.
+print(type(tuple_1))  # Confirm the tuple object type.
+print(tuple_1[2])  # Print the value of the 3rd index position in the tuple.
 
-dict_1 = {'first_key': '1st_val', 'second_key': 2}
-print(type(dict_1))
-print(dict_1['first_key'])
-
-"""1. Create a tuple with strings, floats, and integers.
-2. Confirm the tuple object type.
-3. Print the value of the 3rd index position in the tuple. """
-
-tuple_1 = ('1:00PM', 2.00, '3:PM', 4)
-print(type(tuple_1))
-print(tuple_1[2])
-
-"""1. Create list containing the three object types created above.
-2. Print the value of the first position in the first object (list_1) in the list."""
-
-list_2 = [list_1, dict_1, tuple_1]
-print(list_2[0][0])
-
-
-
-
-
+list_2 = [list_1, dict_1, tuple_1, "hello world"]  # Create list containing the four object types created above.
+print(len(list_2)) # Confirm how many index positions in list_2.
+print(len(list_2[0])) # Confirm how many index positions in the first object in list_2.
+print(list_2[0]) # Print the whole object in the first position of list_2.
+print(list_2[0][0])  # Print the value of the first position in the first object (list_1) in list_2.
+print(list_2[3])  # Print "hello world" from list_2.
