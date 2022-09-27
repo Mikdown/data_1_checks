d5a4c76b7855d1d6533b1737ef622c43ebc2ef2b
MikeDown <mikdowns@outlook.com>
2022-09-27 09:11:59 -0400

    Added code to complete the requirement.

--- data_1_check1.py
+++ data_1_check1.py
@@ -1,7 +1,36 @@
-print('hello world')
+"""1. Print "hello world" with single line of code.
+2. Print "hello world" with variable."""
 
+print('hello world')
 # or like this:
 statement = 'hello world'
 print(statement)
+
+"""1. Create list with strings, floats, and integers.
+2. Confirm the list object type.
+3. Print the value of the 4th position in the list. """
+
+list_1 = ['list1', 'list2', 'list3', 1.10, 2.20, 3.30, 1, 2, 3]
+print(type(list_1))
+print(list_1[3])
+
+"""1. Create dictionary with strings and integers.
+2. Confirm the dictionary object type.
+3. Print the value of the first_key in the dictionary. """
+
+dict_1 = {'first_key': '1st_val', 'second_key': 2}
+print(type(dict_1))
+print(dict_1['first_key'])
+
+"""1. Create a tuple with strings, floats, and integers.
+2. Confirm the tuple object type.
+3. Print the value of the 3rd index position in the tuple. """
+
+tuple_1 = ('1:00PM', 2.00, '3:PM', 4)
+print(type(tuple_1))
+print(tuple_1[2])
+
+
+
 
 
